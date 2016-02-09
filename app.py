a47e491f879ff36de6da25b5e6d51148a062eb59
Aylan Mello <aylanismello@gmail.com>
2016-02-08 23:04:32 -0800

    straightened up app.py and fotoer

--- app.py
+++ app.py
@@ -18,15 +18,6 @@ def index():
 def hi():
 	return 'about that...'
 
-@app.route('/tip')
-def tip():
-	return render_template('tools.html')
-	
-@app.route('/fire')
-def fire():
-	feed = get_feed()
-	# return ("nothing")
-	return render_template('fire.html', infoz=feed)
 
 @app.route('/getfeed', methods=['GET'])
 def get_feed():
@@ -34,52 +25,9 @@ def get_feed():
 	print "got: \n\n\t %s" % infoz
 	return jsonify({'myjson': infoz})
 
-@app.route('/angular')
-def angular():
-	print "GOT YOU"
-	return render_template('/ng-fun.html')
-
-@app.route('/getter', methods=['GET'])
-def getter():
-	return 'yo'
-
-@app.route('/thejson', methods=['GET'])
-def getit():
-	json_file = 'static/test2.json'
-	json_data = open(json_file)
-	data = json.load(json_data)
-	return jsonify({'myjson': data})
-
-
-
-@app.route('/get')
-def getTho():
-	print 'here'
-	return render_template('get.html')
-
-@app.route('/frontend')
-def get_front():
-	#pass our object with all our data....
-	info = [{
-		'ID': '415608',
-		'username': 'Louis The Child',
-		'avatar_url': './artist.png',
-		'track': {
-			'ID': '8533',
-			'title': 'Eyes On U',
-			'url': '/static/eyesOnU.mp3',
-			'artwork_url': 'track.png'
-		}
-	}]
-	return render_template('play.html', info=info)
-	#idea: pass our front end model a json object, and then we work in angular
-
 @app.errorhandler(404)
 def not_found(error):
 	return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-
 
 if __name__ == '__main__':
 	app.run(debug=True)
