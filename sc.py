ad73b2611177c80fce4ce730b14e6295294a6a08
Aylan Mello <aylanismello@gmail.com>
2016-02-10 11:23:13 -0800

    may have fixed noattribute error bug when tracks that are not streamable are accessed

--- sc.py
+++ sc.py
@@ -40,10 +40,9 @@ class SC(object):
 		upperBound = len(self.data['artists'])
 		print "artist num in json file is %d" % upperBound
 
-		for artistIndex in range(numArtistsPerQuery):
-			# get artist access
-
-
+		i = 0
+		while i < numArtistsPerQuery:
+			temp = i
 			artistNum = random.randint(0, upperBound-1)
 			print "go artist num %d" % artistNum
 			artist = self.data['artists'][artistNum]
@@ -77,23 +76,94 @@ class SC(object):
 			print "getting track url of track id: %s " % stringTrackID
 
 			track = client.get('tracks/'+stringTrackID)
-			if track is None:
-				raise AttributeError('fucked up in getting track\n\n\n\n">')
 
-			print "should deal with %s for getting stream_url of %s" % (track, track.stream_url)
+			#MUST CHECK IF TRACK IS STREAMABLE
+			print("is track streamable? %s ") % track.streamable
 
-			stream_url = client.get(track.stream_url, allow_redirects=False)
+			if not track.streamable:
+				print("track not streamable, should get another track\n\n\n\n")
+				i = temp #bug if i is 0
+			else:
 
-			if stream_url is None:
-				raise AttributeError('fucked up in getting stream_url\n\n\n\n">')
 
-			self.artistInfo['track']['url'] = stream_url.location
+				if track is None:
+					raise AttributeError('fucked up in getting track\n\n\n\n">')
 
-			# print "\n\n appending %s" % self.artistInfo
-			self.artistInfos.append(copy.deepcopy(self.artistInfo))
-			# print "appending %s" % self.artistInfo
-			# artistInfos.append(self.artistInfo)
-			# print "the list of dicts so far: %s" % artistInfos
+				print "should deal with %s for getting stream_url of %s" % (track, track.stream_url)
+
+				stream_url = client.get(track.stream_url, allow_redirects=False)
+
+				if stream_url is None:
+					raise AttributeError('fucked up in getting stream_url\n\n\n\n">')
+
+				self.artistInfo['track']['url'] = stream_url.location
+
+				# print "\n\n appending %s" % self.artistInfo
+				self.artistInfos.append(copy.deepcopy(self.artistInfo))
+				i += 1
+				# print "appending %s" % self.artistInfo
+				# artistInfos.append(self.artistInfo)
+				# print "the list of dicts so far: %s" % artistInfos
+
+
+		# for artistIndex in range(numArtistsPerQuery):
+		# 	# get artist access
+		#
+		#
+		# 	artistNum = random.randint(0, upperBound-1)
+		# 	print "go artist num %d" % artistNum
+		# 	artist = self.data['artists'][artistNum]
+		#
+		#
+		#
+		# 	u = client.get('/users', q=artist['username'])
+		#
+		# 	print "got user data of %s" % u
+		#
+		# 	#set artist attributes, id, username, avatar_url
+		# 	self.artistInfo['ID'] = u[0].id
+		# 	self.artistInfo['username'] = u[0].username
+		# 	self.artistInfo['avatar_url'] = u[0].avatar_url
+		# 	# get latest track list by user id
+		# 	t = client.get('/users/'+ str(u[0].id)+ '/tracks')
+		# 	#get id of latest track, etc, etc
+		#
+		# 	trackCount = len(t)
+		# 	print "artist %s has %d tracks " % (self.artistInfo['username'], trackCount)
+		#
+		# 	trackNum = random.randint(0, trackCount - 1)
+		# 	print "selecting track %d" % trackNum
+		#
+		# 	self.artistInfo['track']['ID'] = t[trackNum].id
+		# 	self.artistInfo['track']['title'] = t[trackNum].title
+		# 	self.artistInfo['track']['artwork_url'] = t[trackNum].artwork_url
+		# 	#get track streaming link based on id
+		#
+		# 	stringTrackID = str(t[trackNum].id)
+		# 	print "getting track url of track id: %s " % stringTrackID
+		#
+		# 	track = client.get('tracks/'+stringTrackID)
+		#
+		# 	#MUST CHECK IF TRACK IS STREAMABLE
+		#
+		# 	if(!track.streamable)
+		# 		print("track not streamable, should get another track")
+		#
+		# 	print("is track streamable? %s ") % track.streamable
+		#
+		# 	if track is None:
+		# 		raise AttributeError('fucked up in getting track\n\n\n\n">')
+		#
+		# 	print "should deal with %s for getting stream_url of %s" % (track, track.stream_url)
+		#
+		# 	stream_url = client.get(track.stream_url, allow_redirects=False)
+		#
+		# 	if stream_url is None:
+		# 		raise AttributeError('fucked up in getting stream_url\n\n\n\n">')
+		#
+		# 	self.artistInfo['track']['url'] = stream_url.location
+		#
+		# 	self.artistInfos.append(copy.deepcopy(self.artistInfo))
 
 
 
